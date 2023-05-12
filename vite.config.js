39daffa04b8b1df603a227ba21341ad5483c918c
Siddharth Singh <siddharth.mobcoder@gmail.com>
2023-05-12 16:24:51 +0530

    chore(release):installed some packages for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ssd_portfolio/",
+  base: "/ssd_portfolio",
   plugins: [react()],
 });
